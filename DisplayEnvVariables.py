1aa46f6f3737d9f0746761903be2728dcbff25cc
SathiKondaReddy <55820408+SathiKondaReddy@users.noreply.github.com>
2025-09-09 13:02:40 +0530

    Test

--- DisplayEnvVariables.py
+++ DisplayEnvVariables.py
@@ -1,4 +1,9 @@
 import os
 
 print(f"Failed ECR Dockerfiles: {os.getenv("FAILED_ECR_DOCKERFILES")}")
+print(f"TYPE ECR Dockerfiles: {type(os.getenv("FAILED_ECR_DOCKERFILES"))}")
+
 print(f"Failed ACR Dockerfiles: {os.getenv("FAILED_ACR_DOCKERFILES")}")
+print(f"TYPE ACR Dockerfiles: {type(os.getenv("FAILED_ACR_DOCKERFILES"))}")
+
+
