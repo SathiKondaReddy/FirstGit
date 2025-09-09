d229d5a58004852d114946e2ce948c2f4a557c2b
SathiKondaReddy <55820408+SathiKondaReddy@users.noreply.github.com>
2025-09-09 11:37:19 +0530

    Testing List Variable logic

---
+++ EnvVariableList.py
@@ -0,0 +1,14 @@
+import os
+
+failed_ecr_dockerfiles = []
+
+failed_ecr_dockerfiles.append("base_images/image1.Dockerfile")
+failed_ecr_dockerfiles.append("base_images/image2.Dockerfile")
+
+print(f"Failed ECR Dockerfiles: {failed_ecr_dockerfiles}")
+print(f"Type: {type(failed_ecr_dockerfiles)}")
+
+
+env_file = os.getenv('GITHUB_ENV')
+with open(env_file, "a") as gh_environment:
+    gh_environment.write(f"FAILED_ECR_DOCKERFILES={failed_ecr_dockerfiles}\n")
