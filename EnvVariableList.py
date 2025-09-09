89cecb1ec65cffdd2432d15781e31ac4612954c5
SathiKondaReddy <55820408+SathiKondaReddy@users.noreply.github.com>
2025-09-09 11:55:21 +0530

    Test If

--- EnvVariableList.py
+++ EnvVariableList.py
@@ -1,6 +1,7 @@
 import os
 
 failed_ecr_dockerfiles = []
+failed_acr_dockerfiles = []
 
 failed_ecr_dockerfiles.append("base_images/image1.Dockerfile")
 failed_ecr_dockerfiles.append("base_images/image2.Dockerfile")
@@ -12,3 +13,4 @@ print(f"Type: {type(failed_ecr_dockerfiles)}")
 env_file = os.getenv('GITHUB_ENV')
 with open(env_file, "a") as gh_environment:
     gh_environment.write(f"FAILED_ECR_DOCKERFILES={failed_ecr_dockerfiles}\n")
+    gh_environment.write(f"FAILED_ACR_DOCKERFILES={failed_acr_dockerfiles}\n")
