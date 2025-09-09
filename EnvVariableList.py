7dc1ab65e0c575e12ae9d29b16e0e9b2a437c7ff
SathiKondaReddy <55820408+SathiKondaReddy@users.noreply.github.com>
2025-09-09 13:15:05 +0530

    Test

--- EnvVariableList.py
+++ EnvVariableList.py
@@ -16,5 +16,5 @@ print(f"Type: {type(failed_ecr_dockerfiles)}")
 
 env_file = os.getenv('GITHUB_ENV')
 with open(env_file, "a") as gh_environment:
-    gh_environment.write(f"FAILED_ECR_DOCKERFILES={failed_ecr_dockerfiles}\n")
-    gh_environment.write(f"FAILED_ACR_DOCKERFILES={failed_acr_dockerfiles}\n")
+    gh_environment.write(f"FAILED_ECR_DOCKERFILES={','.join(failed_ecr_dockerfiles)}\n")
+    gh_environment.write(f"FAILED_ACR_DOCKERFILES={','.join(failed_acr_dockerfiles)}\n")
